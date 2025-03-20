d4c6dc762c641e098148e36cf6162a7adebb297e
Harini A <hari628122004@gmail.com>
2025-03-20 23:26:41 +0530

    Create A_Pattern.java
    To print star pattern of "A" as shown below,
    
    **********
    *            *
    *            *
    *            *
    *            *
    **********
    *            *
    *            *
    *            *
    *            *

---
+++ A_Pattern.java
@@ -0,0 +1,16 @@
+public class JavaApplication26 {
+    public static void main(String[] args) {
+        
+        for(int i=1 ; i<11 ; i++){
+            for(int j = 1 ; j <11 ; j++){
+                if((i==6)||(j==1 && i!=1)|| (j==10 && i != 1) || (i==1 && j!=1) || (i==1 && j != 10)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+        
+    }
+}
