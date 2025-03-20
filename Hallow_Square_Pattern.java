a27f1a934b29aa6c06e8fbec437399b598f3bcdb
Harini A <hari628122004@gmail.com>
2025-03-20 20:59:56 +0530

    Create Hallow_Square_Pattern.java
    This code is to print hallow square pattern , as shown below
    
    *  *  *  *  * 
    *             * 
    *             * 
    *             * 
    *  *  *  *  *

---
+++ Hallow_Square_Pattern.java
@@ -0,0 +1,15 @@
+public class JavaApplication26 {
+    public static void main(String[] args) {
+        for (int i=1;i<6;i++){
+            for (int j=1;j<6;j++){
+                if(i==1 || i==5 || j==1 || j==5){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
