60c28b86df4dfeb92e999ee0c3448977785066dd
Harini A <hari628122004@gmail.com>
2025-03-24 21:36:48 +0530

    C_Pattern.java
    C is been printed in star patterns as given below:
    
     * * * * * * * * * 
    *          
    *          
    *          
    *          
    *          
    *          
    *          
    *          
     * * * * * * * * *

--- C_Pattern.java
+++ C_Pattern.java
@@ -1,8 +1,9 @@
-class Hello{
+public class Hello {
+    
   public static void main(String[] args){
     for(int i = 1 ; i < 11 ; i++){
       for(int j = 1 ; j<11 ; j++){
-        if((i==1) || (j==1) || (i==10)){
+        if((i>=2 && i<=9 && j==1) || (i==1 && j>1) || (i==10 && j>1)){
           System.out.print("* ");
         }else{
           System.out.print(" ");
