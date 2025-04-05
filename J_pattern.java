bd04f5bc376a1233a38d36652dfb449f79832075
Harini A <hari628122004@gmail.com>
2025-04-05 20:45:37 +0530

    Create J_pattern.java

---
+++ J_pattern.java
@@ -0,0 +1,17 @@
+public class Learn {
+
+    public static void main(String[] args) {
+        
+        for(int i = 1;i<10;i++){
+            for(int j =1;j<10;j++){
+                if(((i==9) && (j<=5))||(i==1) || (j==5)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }System.out.println();
+        }
+    }
+    
+}
