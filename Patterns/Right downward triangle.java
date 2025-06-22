35d36cb8e58abbde7a983b89b261e4a2529b97e8
Harini A <hari628122004@gmail.com>
2025-06-22 22:59:13 +0530

    Create Right downward triangle.java
    OUTPUT:
    Enter the size of the number
    5
    * * * * * 
    * * * *   
    * * *     
    * *       
    *

---
+++ Patterns/Right downward triangle.java
@@ -0,0 +1,22 @@
+package patterns;
+import java.util.Scanner;
+
+public class Patterns {
+
+ 
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the size of the number");
+     int n = sc.nextInt();
+     for(int i=1;i<=n;i++){
+         for(int j=1;j<=n-(i-1);j++){
+             System.out.print("* ");
+         }for(int k=1;k<i;k++){
+             System.out.print("  ");
+         }
+         System.out.println();
+     }
+        
+    }
+    
+}
