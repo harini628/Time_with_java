8945b0376c1e2535c0a11c16bfb0931f51313cca
Harini A <hari628122004@gmail.com>
2025-07-22 23:54:39 +0530

    Create Main.java

---
+++ Arrays/Stack/Main.java
@@ -0,0 +1,30 @@
+public class Run {
+
+	public static void main(String[] args) {
+		DynamicStack ds = new DynamicStack();
+		ds.push(80);
+		ds.display();
+		ds.push(90);
+		ds.display();
+		ds.push(100);
+		ds.display();
+		ds.push(110);
+		ds.display();
+		ds.push(120);
+		ds.display();
+		ds.pop();
+		ds.display();
+		ds.pop();
+		ds.display();
+		ds.pop();
+		ds.display();
+		ds.pop();
+		ds.display();
+		ds.pop();
+		ds.display();
+		ds.pop();
+		ds.display();
+		
+	}
+
+}
